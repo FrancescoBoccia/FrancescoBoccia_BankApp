a2c8cb322786ee4c9dca164bf50f7030654c2b2d
Boccia <fboccia96@gmail.com>
2021-11-26 00:03:43 +0000

    smooth scrolling added

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,33 +1,55 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 btnsOpenModal.forEach(function (modal) {
-  modal.addEventListener('click', openModal);
+  modal.addEventListener("click", openModal);
 });
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
+});
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+const head = document.querySelector(".header");
+
+message.innerHTML =
+  'This website uses cookies, for a better usage of the website. <button class="btn btn--close-cookie">Got It!!</button>';
+
+head.append(message);
+
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+
+const scrollingButton = document.querySelector(".btn--scroll-to");
+const featuresSection = document.querySelector("#section--1");
+
+scrollingButton.addEventListener("click", function () {
+  featuresSection.scrollIntoView({ behavior: "smooth" });
 });
