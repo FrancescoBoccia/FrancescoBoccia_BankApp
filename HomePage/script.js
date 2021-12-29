9234ec39812c99370732427fbd0b7f0997d4955f
Boccia <fboccia96@gmail.com>
2021-12-29 18:20:35 +0000

    Menu Opacity Fade Animation on Hover

--- HomePage/script.js
+++ HomePage/script.js
@@ -9,9 +9,16 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const message = document.createElement("div");
 const head = document.querySelector(".header");
+const h1 = document.querySelector("h1");
+// Scroll Navigation
 const scrollingButton = document.querySelector(".btn--scroll-to");
 const featuresSection = document.querySelector("#section--1");
-const h1 = document.querySelector("h1");
+// Content Button
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+// Menu Fade Animation on Hover
+const navigationBar = document.querySelector(".nav");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -56,7 +63,7 @@ scrollingButton.addEventListener("click", function () {
   featuresSection.scrollIntoView({ behavior: "smooth" });
 });
 
-// Nav Scroll to Element (Smooth)
+// Click Navigation to Scroll into Element Selected (Smooth)
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.classList.contains("nav__link")) {
@@ -67,14 +74,10 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 
 // /////////////////////////////////////
 // Content Button
-
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 tabsContainer.addEventListener("click", function (e) {
   e.preventDefault();
 
+  // Highlight the title you have clicked
   const clicked = e.target.closest(".operations__tab");
   if (!clicked) return;
   tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
@@ -82,7 +85,29 @@ tabsContainer.addEventListener("click", function (e) {
     content.classList.remove("operations__content--active")
   );
   clicked.classList.add("operations__tab--active");
+
+  // Select which content to show
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+// /////////////////////////////////////
+// Menu Opacity Fade Animation on Hover
+const navLinksHoverOpacity = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const linkMouseOver = e.target;
+    const siblingsLinks = linkMouseOver
+      .closest(".nav")
+      .querySelectorAll(".nav__link");
+    const logo = linkMouseOver.closest(".nav").querySelector("img");
+
+    siblingsLinks.forEach((el) => {
+      if (el !== linkMouseOver) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+navigationBar.addEventListener("mouseover", navLinksHoverOpacity.bind(0.5));
+navigationBar.addEventListener("mouseout", navLinksHoverOpacity.bind(1));
